1753bdeb30dfc1fe7e91326bc9f1435da748beea
roc <roc.liu@allchips.com>
2021-01-05 16:15:18 +0800

    修改bug

--- src/main/java/udf/UDFConcatJSONArray.java
+++ src/main/java/udf/UDFConcatJSONArray.java
@@ -1,6 +1,5 @@
 package udf;
 
-import netscape.javascript.JSException;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,14 +15,14 @@ import java.util.ArrayList;
  * 版本:v1.0
  */
 public class UDFConcatJSONArray extends UDF {
-    public String evaluate(String JSONString, String param) {
+    public String evaluate(String JSONString, String param, String brand) {
         //判断param是否为null
-        if (param == null) {
-            return JSONString;
+        if (JSONString == null) {
+            return null;
         }
 
-        if (param.equals("")) {
-            return JSONString;
+        if (JSONString.equals("")) {
+            return null;
         }
 
         try {
@@ -38,6 +37,12 @@ public class UDFConcatJSONArray extends UDF {
             jsonObject.put("property_name", "封装 / 箱体");
             jsonObject.put("property_value", param);
             result.add(jsonObject.toString());
+
+            //把品牌处理成JSON格式，添加到属性中
+            JSONObject jsonBrand = new JSONObject();
+            jsonBrand.put("property_name", "品牌");
+            jsonBrand.put("property_value", brand);
+            result.add(jsonBrand.toString());
             return result.toString();
         } catch (JSONException e) {
             System.out.println("JSON字符串格式错误！");
